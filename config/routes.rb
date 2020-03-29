6c5327bdf0998cef724dc677ed603fa6673d7c46
Fabien Davy <davyfabien@gmail.com>
2020-03-29 21:57:25 +0200

    cleaned up the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'pages#home'
-  get "/dashboard", to: "pages#dashboard"
-  get "/join", to: "games#join"
-  post "", to: "games#redirect", as: :redirect
-
-  resources :games, only: [:create, :show]
-
 end
