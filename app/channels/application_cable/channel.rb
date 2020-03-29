745891f19c2fd248c1e7678b61c978f021ed1e07
Fabien Davy <davyfabien@gmail.com>
2020-03-29 22:50:32 +0200

    started to make subscription

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,9 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    identified_by :uuid
+
+    def connect
+      self.uuid = SecureRandom.urlsafe_base64
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'pages#home'
+  
+  mount ActionCable.server => "/cable"
+
 end
